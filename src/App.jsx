8961a25fb6e680c45b99a2345271b732a4380cbb
Ujwal <ujwalkurhade05@gmail.com>
2023-10-02 15:37:31 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -7,28 +7,34 @@ const initialNodes = [
   {
     id: "1",
     position: { x: 0, y: 0 },
-    data: { label: "1" },
+    data: { label: "start" },
   },
   {
     id: "2",
     position: { x: 0, y: 100 },
-    data: { label: "2" },
+    data: { label: "node1" },
   },
   {
     id: "3",
     position: { x: 0, y: 200 },
-    data: { label: "3" },
+    data: { label: "node2" },
   },
   {
     id: "4",
     position: { x: 200, y: 100 },
-    data: { label: "4" },
+    data: { label: "node4" },
+  },
+  {
+    id: "5",
+    position: { x: 200, y: 300 },
+    data: { label: "node5" },
   },
 ];
 const initialEdges = [
   { id: "e1-2", source: "1", target: "2" },
   { id: "e1-2", source: "2", target: "3" },
   { id: "e1-2", source: "1", target: "4" },
+  { id: "e1-2", source: "5", target: "4" },
 ];
 
 export default function App() {
